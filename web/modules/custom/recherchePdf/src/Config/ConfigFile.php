9cfbe3feda42fcc41c6a130ad0c8fe3d689d1e05
abhmidi <abhmidi@users.noreply.github.com>
2018-05-02 15:01:04 +0200

    module recherche

---
+++ web/modules/custom/recherchePdf/src/Config/ConfigFile.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: abh
+ * Date: 02/05/2018
+ * Time: 11:41
+ */
+
+namespace Drupal\recherchePdf\Config;
+class ConfigFile
+{
+    public const NOM_SITE = 'TURBOCAR distributeur auto, moto,securite auto - produits M6 Turbo';
+    public const URL_SITE = 'http://www.turbocar-entretien.com';
+    public const ID_SOC = 37;
+    public const DEFAULT_PDF = 'Lang';
+    public const DEFAULT_LG = 'Fr';
+    public const DEFAULT_DIR = 'Det';
+    public const CODE_SOC = 'TBC';
+}
--- web/modules/custom/recherchePdf/src/Controller/ResultController.php
+++ web/modules/custom/recherchePdf/src/Controller/ResultController.php
@@ -10,20 +10,54 @@ namespace Drupal\recherchePdf\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class ResultController extends ControllerBase
 
 {
+    protected $config;
+
+
+    public function __construct()
+    {
+        $this->config = new \Drupal\recherchePdf\Config\ConfigFile();
+
+    }
+
     public function getPdf(Request $request)
     {
+        $refProduit = $request->request->get('refProduit');
+        $lotProduit = $request->request->get('lotProduit');
 
-        //$build['test'] = ['#markup' => $request->query->get('result_pdf')];
+        $path = $this->getPdfByRefOrLot($refProduit, $lotProduit);
+        $url = $this->config::URL_SITE . "/" . $this->config::DEFAULT_PDF . "/" . $this->config::DEFAULT_LG . "/" . $this->config::DEFAULT_DIR . "/" . $path[0]->name_fic;
+        return new JsonResponse($url, 200, ['Content-Type' => 'application/json']);
 
-        return array(
-            '#theme' => 'resultat',
-            //'#texte' => $this->t('Hello world !'),
-            '#fiche_pdf' => $request->query->get('fiche_pdf')
-        );
+    }
 
+    public function getPdfByRefOrLot($refProduit = NULL, $lotProduit = FALSE)
+    {
+        // Switch to external database
+        \Drupal\Core\Database\Database::setActiveConnection('QRcodeTBC');
+
+        $db = \Drupal\Core\Database\Database::getConnection();
+
+        // $connection = \Drupal::database();
+
+        if ($refProduit && strlen($lotProduit == 0)) {
+            $req = "select name_fic from fiches_produit where soc_id='" . $this->config::ID_SOC . "' and RefProduit='" . $refProduit . "' and LNGProduit = '" . $this->config::DEFAULT_LG . "' limit 1";
+            $query = $db->query($req);
+        } elseif ($lotProduit && strlen($refProduit == 0)) {
+            $req = "select name_fic from fiches_produit where soc_id='" . $this->config::ID_SOC . "' and LotProduit='" . $lotProduit . "' and LNGProduit = '" . $this->config::DEFAULT_LG . "' limit 1";
+            $query = $db->query($req);
+        } else {
+            $req = "select name_fic from fiches_produit where soc_id='" . $this->config::ID_SOC . "' and RefProduit='" . $refProduit . "' and LotProduit='" . $lotProduit . "' and LNGProduit = '" . $this->config::DEFAULT_LG . "' limit 1";
+            $query = $db->query($req);
+        }
+        $result = $query->fetchAll();
+
+        // Switch default database
+        \Drupal\Core\Database\Database::setActiveConnection();
+        return $result;
     }
 }

5c2ae8f5376c56a3262866c7b5d5de6f89867f65
Jean-Marc Viguie <jm.viguie@cargo-services.fr>
2018-05-07 15:22:05 +0200

    Mise à jour du module « recherchePdf »
    Le module utilise l’api de l’application « Créer et Gérer vos QR Codes, Fiches ».
    class « ConfigFile » les paramètres de la société Turbo Car sont à jour. 
    Pour tous les sites qui utilisent le module « recherchePdf » il ne faut pas oublier d’ajouter les paramètres de la société dans le fichier class « ConfigFile » et définir dans le fichier setting du site la clef de la société.
    Exemple : $config['system.qrcodeadm']['soc'] = 'TBC';

--- web/modules/custom/recherchePdf/src/Controller/RechercheController.php
+++ web/modules/custom/recherchePdf/src/Controller/RechercheController.php
@@ -13,6 +13,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\BinaryFileResponse;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 
 class RechercheController extends ControllerBase
 
@@ -32,43 +33,64 @@ class RechercheController extends ControllerBase
             '#theme' => 'recherche',
         );
     }
-
     public function getPdf(Request $request)
     {
+
         $refProduit = $request->get('refProduit');
         $lotProduit = $request->get('lotProduit');
-        $rslt =$this->getPdfByRefOrLot($refProduit, $lotProduit);
-        if (count($rslt) > 0) {
-            $fileName = $rslt[0]->name_fic;
+        $qrcodeadm =  \Drupal::config('system.qrcodeadm')->get('soc', FALSE);
+        $configQrcodeadm = false;
 
-            $url = $this->config::URL_SITE . "/" . $this->config::DEFAULT_PDF . "/" . $this->config::DEFAULT_LG . "/" . $this->config::DEFAULT_DIR . "/" . $fileName;
-            if ($this->is_url_exist($url)) {
-                $response = new Response();
-                $response->headers->set('Content-type', 'application/octet-stream');
-                $response->headers->set('Content-Disposition', sprintf('attachment; filename="%s"', $fileName));
-                $response->setContent(file_get_contents($url));
-                $response->setStatusCode(200);
-                $response->headers->set('Content-Transfer-Encoding', 'binary');
-                $response->headers->set('Pragma', 'no-cache');
-                $response->headers->set('Expires', '0');
-                return $response;
+        $dataSoc = $this->config::QRCODEADM[$qrcodeadm];
 
-            } else {
-                return array(
-                    '#theme' => 'recherche',
-                    '#msg' => $this->t('Fiche de données sécurité non trouvée merci de vérifier votre saisie'),
-                );
-            }
-        } else {
-            return array(
-                '#theme' => 'recherche',
-                '#msg' => $this->t('Fiche de données sécurité non trouvée merci de vérifier votre saisie'),
-            );
+        if($qrcodeadm != null) {
+          $configQrcodeadm = true;
         }
+
+        $return = array(
+          '#theme' => 'recherche',
+          '#msg' => $this->t('Une erreur système ne nous permet pas de donnée suite à votre demande.'),
+        );
+
+        $postData = [
+          'RefProd' => $refProduit,
+          'LotProd' => $lotProduit,
+          'ID_SOC' => $dataSoc['ID_SOC'],
+          'CODE_SOC' => $dataSoc['CODE_SOC'],
+          'DEFAULT_PDF' => $dataSoc['DEFAULT_PDF'],
+          'DEFAULT_LG' => $dataSoc['DEFAULT_LG'],
+          'DEFAULT_DIR' => $dataSoc['DEFAULT_DIR'],
+          'tri_soc' => $dataSoc['tri_soc'],
+        ];
+        if($qrcodeadm == true) {
+          $rslt = $this->getPdfByRefOrLotCurl($postData);
+          if ($rslt != null && $rslt->nomfic != '') {
+            $url = $dataSoc['URL_SITE'] . "/" . $rslt->nomfic;
+            if ($this->is_url_exist($url)) {
+              return $this->headersResponse($url, $rslt->fileName);
+            } else {
+              $this->redirectResponse(
+                  '/recherche-pdf',
+                  $this->t('Fiche de données sécurité non trouvée merci de vérifier votre saisie')
+              );
+            }
+          } else {
+            $this->redirectResponse(
+              '/recherche-pdf',
+              $this->t('Fiche de données sécurité non trouvée merci de vérifier votre saisie')
+            );
+          }
+        }
+        $return = array(
+          '#theme' => 'recherche'
+        );
+
+      return $return;
     }
 
     public function getPdfByRefOrLot($refProduit = '', $lotProduit = '')
     {
+
         // Switch to external database
         \Drupal\Core\Database\Database::setActiveConnection('QRcodeTBC');
 
@@ -93,6 +115,59 @@ class RechercheController extends ControllerBase
 
         return $result;
     }
+    public function _getPdf(Request $request)
+  {
+    $refProduit = $request->get('refProduit');
+    $lotProduit = $request->get('lotProduit');
+    $rslt =$this->getPdfByRefOrLot($refProduit, $lotProduit);
+    if (count($rslt) > 0) {
+      $fileName = $rslt[0]->name_fic;
+
+      $url = $this->config::URL_SITE . "/" . $this->config::DEFAULT_PDF . "/" . $this->config::DEFAULT_LG . "/" . $this->config::DEFAULT_DIR . "/" . $fileName;
+      if ($this->is_url_exist($url)) {
+        $response = new Response();
+        $response->headers->set('Content-type', 'application/octet-stream');
+        $response->headers->set('Content-Disposition', sprintf('attachment; filename="%s"', $fileName));
+        $response->setContent(file_get_contents($url));
+        $response->setStatusCode(200);
+        $response->headers->set('Content-Transfer-Encoding', 'binary');
+        $response->headers->set('Pragma', 'no-cache');
+        $response->headers->set('Expires', '0');
+        return $response;
+
+      } else {
+        return array(
+          '#theme' => 'recherche',
+          '#msg' => $this->t('Fiche de données sécurité non trouvée merci de vérifier votre saisie'),
+        );
+      }
+    } else {
+      return array(
+        '#theme' => 'recherche',
+        '#msg' => $this->t('Fiche de données sécurité non trouvée merci de vérifier votre saisie'),
+      );
+    }
+  }
+
+  public function getPdfByRefOrLotCurl($postfields = [])
+    {
+        $result = null;
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, $this->config::API_URL);
+        curl_setopt($curl, CURLOPT_COOKIESESSION, true);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_POST, true);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $postfields);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+            'Content-Type: application/json',
+            'Content-Length: ' . strlen($postfields))
+        );
+        $return = curl_exec($curl);
+        curl_close($curl);
+        $return = \GuzzleHttp\json_decode($return);
+
+        return $return;
+    }
 
     protected function is_url_exist($url)
     {
@@ -109,4 +184,26 @@ class RechercheController extends ControllerBase
         curl_close($ch);
         return $status;
     }
+
+    protected function headersResponse($url,$fileName)
+    {
+        $response = new Response();
+        $response->headers->set('Content-type', 'application/octet-stream');
+        $response->headers->set('Content-Disposition', sprintf('attachment; filename="%s"', $fileName));
+        $response->setContent(file_get_contents($url));
+        $response->setStatusCode(200);
+        $response->headers->set('Content-Transfer-Encoding', 'binary');
+        $response->headers->set('Pragma', 'no-cache');
+        $response->headers->set('Expires', '0');
+        return $response;
+    }
+
+    protected function redirectResponse($url,$message)
+    {
+      $messenger = \Drupal::messenger();
+      $messenger->addMessage($message,'error', false);
+
+      return new RedirectResponse($url);
+    }
+
 }

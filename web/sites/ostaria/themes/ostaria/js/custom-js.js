03cf63008741a790308a53a1d58036edd6ef0cd5
Nicolas <n.troger@cargo-services.fr>
2018-07-13 16:24:56 +0200

    Inté autour de la navigation et du header

--- web/sites/ostaria/themes/ostaria/js/custom-js.js
+++ web/sites/ostaria/themes/ostaria/js/custom-js.js
@@ -1,9 +1,31 @@
 jQuery(document).ready(function($) {
-    /* Test */
-    $('.toggle-menu').click(function() {
+    /* Test Navigation */
+    var openNav = function () {
+        $('.overlay').fadeToggle();
+        $('.navigation').toggleClass('opened');
+        $('.nav-inner').fadeIn();
+        $('.nav-inner').toggleClass('in-left');
+        $('.menu-link ').toggleClass('active');
+        $('.button-wrapper').toggleClass('is-active');
+        isNavOpen = true;
+    };
+    var closeNav = function () {
         $('.overlay').fadeToggle();
         $('.navigation').toggleClass('opened');
         $('.nav-inner').toggleClass('in-left');
-        $(this).toggleClass('active');
+        $('.nav-inner').fadeOut();
+        $('.menu-link').toggleClass('active');
+        $('.button-wrapper').toggleClass('is-active');
+        isNavOpen = false;
+    };
+
+    var isNavOpen = false;
+
+    $('.js-toggle-menu').click(function() {
+        if (isNavOpen === false) {
+            openNav();
+        } else {
+            closeNav();
+        }
     });
 });

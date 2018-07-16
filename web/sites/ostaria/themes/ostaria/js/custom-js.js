5edf0ab0d28df0c2d2d746d622be0e0e719f2a26
Nicolas <n.troger@cargo-services.fr>
2018-07-16 15:02:10 +0200

    Home

--- web/sites/ostaria/themes/ostaria/js/custom-js.js
+++ web/sites/ostaria/themes/ostaria/js/custom-js.js
@@ -1,10 +1,11 @@
 jQuery(document).ready(function($) {
-    /* Test Navigation */
+    /** Navigation **/
+    /* Functions */
     var openNav = function () {
         $('.overlay').fadeToggle();
         $('.navigation').toggleClass('opened');
         $('.nav-inner').fadeIn();
-        $('.nav-inner').toggleClass('in-left');
+        $('.nav-inner').toggleClass('in-left-menu');
         $('.menu-link ').toggleClass('active');
         $('.button-wrapper').toggleClass('is-active');
         isNavOpen = true;
@@ -12,15 +13,15 @@ jQuery(document).ready(function($) {
     var closeNav = function () {
         $('.overlay').fadeToggle();
         $('.navigation').toggleClass('opened');
-        $('.nav-inner').toggleClass('in-left');
+        $('.nav-inner').toggleClass('in-left-menu');
         $('.nav-inner').fadeOut();
         $('.menu-link').toggleClass('active');
         $('.button-wrapper').toggleClass('is-active');
         isNavOpen = false;
     };
 
+    /* Actions */
     var isNavOpen = false;
-
     $('.js-toggle-menu').click(function() {
         if (isNavOpen === false) {
             openNav();
@@ -29,3 +30,41 @@ jQuery(document).ready(function($) {
         }
     });
 });
+
+(function ($, window, document, undefined) {
+    'use strict';
+    var animationObject;
+
+    function nvsAddAnimation() {
+        animationObject.each(function (index, element) {
+            var $currentElement = $(element),
+                animationType = $currentElement.attr('nvs-animation-type');
+
+            if (nvsOnScreen($currentElement)) {
+                $currentElement.addClass('animated ' + animationType);
+            }
+        });
+    }
+
+    // takes jQuery(element) a.k.a. $('element')
+    function nvsOnScreen(element) {
+        // window bottom edge
+        var windowBottomEdge = $(window).scrollTop() + $(window).height();
+
+        // element top edge
+        var elementTopEdge = element.offset().top;
+        var offset = 100;
+
+        // if element is between window's top and bottom edges
+        return elementTopEdge + offset <= windowBottomEdge;
+    }
+
+    $(window).on('load', function () {
+        animationObject = $('[nvs-animation-type]');
+        nvsAddAnimation();
+    });
+
+    $(window).on('scroll', function (e) {
+        nvsAddAnimation();
+    });
+}(jQuery, window, document));

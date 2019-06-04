5ae8fdc0c343bd8c00d4a7ffce3cdffe5dfd7e5f
Nicolas <n.troger@cargo-services.fr>
2019-06-04 14:25:54 +0200

    Squash - INT
    Modif gestion bloc texte banner + début inté page Marque
    
    
    Page Marque + Nav mobile/tablet
    
    
    Squash commit
    
    Ajout arrows nav mobile
    
    
    Footer mobile
    
    
    Page LookBook
    
    
    Pages : "Lookbook" et "Devenir Distributeur"
    
    Page "Devenir Distributeur"
    
    
    Page Contact
    
    
    Parutions presse
    
    
    tweaks page parutions presse
    
    
    PDF dans popup
    
    
    Fix pour IE11
    
    
    Fix footer

--- web/sites/comptoirdefamille/themes/comptoirdefamille/js/custom-js.js
+++ web/sites/comptoirdefamille/themes/comptoirdefamille/js/custom-js.js
@@ -3,36 +3,24 @@ jQuery(document).ready(function($) {
     var isMobile = window.matchMedia("only screen and (max-width: 767px)").matches;
     var isTabletOrLess = window.matchMedia("only screen and (max-width: 991px)").matches;
 
+    // FAKE LINK FOR SUBMENUS
+    $(".js-fake-link").click(function (e) {
+        e.preventDefault();
+    });
+
     // NAV MOBILE
-    if ( isTabletOrLess ) {
-        var $menu = $("#block-groupecargo-main-menu").mmenu({
-            "extensions": [
-                "theme-white",
-                "border-full",
-                "position-front",
-                "pagedim-black",
-                "fullscreen",
-                "position-right"
-            ]
-        }, {
-            clone: true
+    if (isTabletOrLess) {
+        $(".navbar-toggler").click(function() {
+            $(this).toggleClass("is-active");
+            $("#nav-wrapper").toggleClass("active");
+            $("#content-wrap > .container-fluid").toggleClass("disable");
+            $("body").toggleClass("disable");
         });
 
-        var $icon = $(".navbar-toggler");
-        var API = $menu.data("mmenu");
-
-        API.bind( "open:start", function() {
-            setTimeout(function() {
-                $icon.addClass( "is-active" );
-                $icon.on("click",function() {
-                    API.close();
-                })
-            }, 100);
-        });
-        API.bind( "close:start", function() {
-            setTimeout(function() {
-                $icon.removeClass( "is-active" );
-            }, 100);
+        $("#nav-wrapper a.dd").click(function(e) {
+            e.preventDefault();
+            $(this).toggleClass("opened");
+            $(this).parent().find("ul").slideToggle();
         });
     }
 
@@ -43,5 +31,60 @@ jQuery(document).ready(function($) {
         dots: true,
         fade: true,
         cssEase: 'linear'
+    });
+
+    // LA MARQUE - Slick
+    var marqueSlider = $(".js-slides-ambiances > div > div:nth-child(2)");
+    marqueSlider.slick({
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+
+    // PAGE CONTACT
+    if ($('.webform-submission-form').length) {
+        // Function
+        var checkValue = function (e) {
+            tmpval = e.val();
+            if (tmpval === '') {
+                e.addClass('js-empty').removeClass('js-not-empty');
+            } else {
+                e.addClass('js-not-empty').removeClass('js-empty');
+            }
+        };
+        // Var Elts
+        var formElts = $('input, textarea');
+        // 1er chargement
+        formElts.each(function () {
+            checkValue($(this));
+        });
+        // Au clic sur l'elt
+        formElts.blur(function () {
+            checkValue($(this));
+        });
+        // Required
+        $(".required").parent().addClass("required-wrapper");
+    }
+
+    // PARUTION PRESSE
+    $(".js-link-pdf").click(function () {
+        var link = $(this).attr("data-link");
+        $(".js-pdf-embed").attr("src",link);
     });
 });

a5792b7fb6f71b5a4931f66580fb942da28e2ea4
Audrey <audreybeziat@yahoo.fr>
2019-07-11 11:05:58 +0200

    Update fichier config avec nouvelle version api
    Nouvelle version de l'api admin qrcode
    Des informations sont inutiles donc suppression de quelques lignes dans le fichier config
    clean du fichier config car pb/diff lors du rebase

--- web/modules/custom/recherche_pdf/src/Config/ConfigFile.php
+++ web/modules/custom/recherche_pdf/src/Config/ConfigFile.php
@@ -3,42 +3,36 @@
 namespace Drupal\recherche_pdf\Config;
 class ConfigFile
 {
-//    CONST API_URL = 'http://admin.url-qrcode.com/api/'; /* L'url curl api */
-    CONST API_URL = 'http://admin.url-qrcode.com/api/index-audrey-test-2.php'; /* L'url curl api test audrey */
+    CONST API_URL = 'http://admin.url-qrcode.com/api/fiches.php'; /* L'url curl api */
+//    CONST API_URL = 'http://admin.url-qrcode.com/api/index-audrey-test-2.php'; /* L'url curl api test audrey */
     CONST QRCODEADM = [
-      'TBC' => [
-          'URL_SITE' => 'http://fiches.turbocar-entretien.com',
-          'ID_SOC' => 37,
-          'DEFAULT_PDF' => 'Lang',
-          'DEFAULT_LG' => 'Fr',
-//          'EMAIL_FIC_TO' => ['poleweb@cargo-services.fr','a.delpoux@cargo-services.fr'],
-          'EMAIL_FIC_TO' => ['a.beziat@cargo-services.fr'],
-          'EMAIL_FIC_FROM' => 'no-reply@turbocar-sas.com',
-      ],
-      'FCA' => [
-          'URL_SITE' => 'http://fiches.facom-colle-adhesif.com',
-          'ID_SOC' => 47,
-          'DEFAULT_PDF' => 'Lang',
-          'DEFAULT_LG' => 'Fr',
-<<<<<<< develop:web/modules/custom/recherchePdf/src/Config/ConfigFile.php
-          'DEFAULT_DIR' => 'Fds',
-          'tri_soc' => 'faca',
-          'EMAIL_FIC_TO' => ['poleweb@cargo-services.fr','a.delpoux@cargo-services.fr'],
-=======
-//          'EMAIL_FIC_TO' => ['poleweb@cargo-services.fr','a.delpoux@cargo-services.fr'],
-          'EMAIL_FIC_TO' => ['a.beziat@cargo-services.fr'],
->>>>>>> Création de cogex epi + Améliorer le module de recherche pdf:web/modules/custom/recherche_pdf/src/Config/ConfigFile.php
-          'EMAIL_FIC_FROM' => 'no-reply@facom-colle-adhesif.com',
-      ],
-      'CGXEPI' => [
-          'URL_SITE' => 'http://ficheepi.cogex-outillage.com/',
-          'ID_SOC' => 51,
-          'DEFAULT_PDF' => 'Lang',
-          'DEFAULT_LG' => 'Fr',
-//          'EMAIL_FIC_TO' => ['poleweb@cargo-services.fr','a.delpoux@cargo-services.fr'],
-          'EMAIL_FIC_TO' => ['a.beziat@cargo-services.fr'],
-          'EMAIL_FIC_FROM' => 'no-reply@cogex-outillage.com',
-      ],
+        'TBC' => [
+            'URL_SITE' => 'http://fiches.turbocar-entretien.com',
+            'ID_SOC' => 37,
+            'DEFAULT_PDF' => 'Lang',
+            'DEFAULT_LG' => 'Fr',
+            //          'EMAIL_FIC_TO' => ['poleweb@cargo-services.fr','a.delpoux@cargo-services.fr'],
+            'EMAIL_FIC_TO' => ['a.beziat@cargo-services.fr'],
+            'EMAIL_FIC_FROM' => 'no-reply@turbocar-sas.com',
+        ],
+        'FCA' => [
+            'URL_SITE' => 'http://fiches.facom-colle-adhesif.com',
+            'ID_SOC' => 47,
+            'DEFAULT_PDF' => 'Lang',
+            'DEFAULT_LG' => 'Fr',
+//            'EMAIL_FIC_TO' => ['poleweb@cargo-services.fr','a.delpoux@cargo-services.fr'],
+            'EMAIL_FIC_TO' => ['a.beziat@cargo-services.fr'],
+            'EMAIL_FIC_FROM' => 'no-reply@facom-colle-adhesif.com',
+        ],
+        'CGXEPI' => [
+            'URL_SITE' => 'http://ficheepi.cogex-outillage.com/',
+            'ID_SOC' => 51,
+            'DEFAULT_PDF' => 'Lang',
+            'DEFAULT_LG' => 'Fr',
+            //          'EMAIL_FIC_TO' => ['poleweb@cargo-services.fr','a.delpoux@cargo-services.fr'],
+            'EMAIL_FIC_TO' => ['a.beziat@cargo-services.fr'],
+            'EMAIL_FIC_FROM' => 'no-reply@cogex-outillage.com',
+        ],
 
     ];
 }

7f99868d13efd34f2f9c1ce608826488315e84be
Nicolas <n.troger@cargo-services.fr>
2019-08-27 16:04:03 +0200

    Page contact

--- web/sites/cestdeuxeuros/themes/cestdeuxeuros/js/custom-js.js
+++ web/sites/cestdeuxeuros/themes/cestdeuxeuros/js/custom-js.js
@@ -1,3 +1,24 @@
 jQuery(document).ready(function($) {
-
+    // PAGE CANDIDATURE SPONTANEE
+    if ($('.webform-submission-form').length) {
+        // Function
+        var checkValue = function (e) {
+            tmpval = e.val();
+            if (tmpval === '') {
+                e.addClass('js-empty').removeClass('js-not-empty');
+            } else {
+                e.addClass('js-not-empty').removeClass('js-empty');
+            }
+        };
+        // Var Elts
+        var formElts = $('input, textarea');
+        // 1er chargement
+        formElts.each(function () {
+            checkValue($(this));
+        });
+        // Au clic sur l'elt
+        formElts.blur(function () {
+            checkValue($(this));
+        });
+    }
 });

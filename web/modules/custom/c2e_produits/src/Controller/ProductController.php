cba3736899cffa88882a7958ed092b0783e47fba
Audrey <a.beziat@cargo-services.fr>
2019-08-28 11:08:42 +0200

    Flux RSS produits + images créé

--- web/modules/custom/c2e_produits/src/Controller/ProductController.php
+++ web/modules/custom/c2e_produits/src/Controller/ProductController.php
@@ -2,17 +2,9 @@
 namespace Drupal\c2e_produits\Controller;
 
 
-use Drupal\Core\Controller\ControllerBase;
 
-class ProductController extends ControllerBase
+class ProductController extends FonctionsController
 {
-    private $filesPath;
-
-    public function __construct()
-    {
-        $this->filesPath = \Drupal::service('file_system')->realpath(file_default_scheme() . "://");
-    }
-
     public function displayProducts($week, $nbProducts)
     {
         $products = $this->getCsvContent();
@@ -25,40 +17,5 @@ class ProductController extends ControllerBase
         }
 
         return $imgsProducts;
-    }
-
-    private function getCsvContent()
-    {
-        $ficCSV = $this->filesPath.'/datacsv/ARTICLES_WEB.CSV';
-        $arr = [];
-
-        $handle = fopen($ficCSV, "r");
-        if ($handle) {
-            while (($line = fgets($handle)) !== false) {
-                $l=explode(';',$line);
-                if (in_array(trim($l[1]), ["S0","S1","S2","S3"])) $arr[trim($l[1])][] = trim($l[0]);
-            }
-            fclose($handle);
-        }
-
-        return $arr;
-    }
-
-    private function getPictureFileNameOfSku($sku)
-    {
-        $picturesPath = $this->filesPath.'/dataimages/';
-
-        $filenameEmb = $sku."_EMB_BD.jpg";
-        $filenameDeb = $sku."_DEB_BD.jpg";
-
-        if (file($picturesPath.$filenameDeb)) {
-            $return = $filenameDeb;
-        } elseif (file($picturesPath.$filenameEmb)) {
-            $return = $filenameEmb;
-        } else {
-            $return = "";
-        }
-
-        return $return;
     }
 }

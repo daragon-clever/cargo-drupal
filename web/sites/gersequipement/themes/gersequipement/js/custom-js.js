4d9c37613920aa55b1886cdbfb17fc320e21f506
Nicolas <n.troger@cargo-services.fr>
2019-10-31 10:20:32 +0100

    Début intégration

--- web/sites/gersequipement/themes/gersequipement/js/custom-js.js
+++ web/sites/gersequipement/themes/gersequipement/js/custom-js.js
@@ -1,3 +1,42 @@
 jQuery(document).ready(function($) {
+    // NAV
+    var timerNav;
+    $(".header-wrapper nav").hover(
+        function() {
+            timerNav = setTimeout(function() {
+                $(".header-wrapper").addClass("menu-hover");
+            }, 400);
+        },
+        function() {
+            $(".header-wrapper").removeClass("menu-hover");
+            clearTimeout(timerNav);
+        }
+    );
 
+    // SLIDER HP
+    var sliderHPLeft = $(".js-slick-slider-hp-left > div");
+    var sliderHPRight = $(".js-slick-slider-hp-right > div");
+
+    sliderHPLeft.slick({
+        vertical: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        speed: 1200
+    });
+
+    sliderHPRight.slick({
+        vertical: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        arrows: false,
+        verticalReverse: true,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        speed: 1200
+    });
 });

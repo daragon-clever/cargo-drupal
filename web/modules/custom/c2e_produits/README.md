3a748d96ee3232fa307e22b213ac97d27bd2bfd8
Audrey <a.beziat@cargo-services.fr>
2020-03-02 16:44:17 +0100

    ajout desc + transformer en command au lieu de cron par defaut drupal
    clean fichier inutile
    
    
    MAJ readme
    
    
    Changer description command check product

--- web/modules/custom/c2e_produits/README.md
+++ web/modules/custom/c2e_produits/README.md
@@ -3,3 +3,20 @@
 Ce module affiche les produits de **S0** (semaine actuelle) et **S1** (semaine prochaine) grâce à des fichiers 
 générés dans le dossier **files/dataimages** et **files/datacsv**.
 Il y a également un **flux rss** de présent pour générer un flux pour S0 et S1 (principalement pour Actito).
+
+## Commande
+
+2 commandes dans ce module :
+- c2e_produits:check : contrôle si tous les produits sont présents, ainsi que les images, pour
+S0 et S1
+- c2e_produits:generate-flux-rss : créer le flux RSS, utilisé dans Actito
+
+## Cron
+
+Un cron exécute ces 2 commandes à 7h15
+
+## ++
+
+Exécuter sur sa VM : 
+`docker-compose run --rm php vendor/bin/drupal --uri=web.cestdeuxeuros.svdXpweb-stm.ressinfo.ad`
+avec la commande qui suite.  Pensez à remplacer le X par le numéro de votre VM.
--- web/modules/custom/c2e_produits/src/Controller/ProductController.php
+++ web/modules/custom/c2e_produits/src/Controller/ProductController.php
@@ -2,71 +2,164 @@
 namespace Drupal\c2e_produits\Controller;
 
 
-class ProductController extends FonctionsController
+use Drupal\Core\Controller\ControllerBase;
+
+class ProductController extends ControllerBase
 {
+    public const KEY_IMG_PATH = "img";
+
+    private const KEY_ALERT_OBJ = "obj";
+    private const KEY_ALERT_TXT = "txt";
+
     private const NB_REQUIRED_PRODUCTS = 8;
     private const EMAIL_TEAM = "poleweb@cargo-services.fr";
+//    private const EMAIL_ALERT = "gel@cedif.fr,sgu@cedif.fr,a.champion@cargo-services.fr";
+    private const EMAIL_ALERT = "a.beziat@cargo-services.fr"; //test mode
 
-    private $error = "";
+    private $helperFct;
 
-    public function displayProducts(string $week, int $nbProducts): ?array
+    private $csvContent;
+    private $lastUpdatedCsv;
+
+    private $error = [];
+
+    public function __construct()
     {
-        $products = $this->getCsvContent();
-        $productsOfTheWeek = $products[$week];
+        $this->helperFct = new MyFunctionsController();
+        $this->csvContent = $this->helperFct->getCsvContent();
+        $this->lastUpdatedCsv = $this->getCsvTime();
+    }
+
+    private function getCsvTime(): string
+    {
+        $lastUpdatedFile = date('Y-m-d', filemtime($this->helperFct->csvFile));
+
+        return $lastUpdatedFile;
+    }
+
+    public function check(string $week): bool
+    {
+        $week = strtoupper($week);
+
+        $checkNbProducts = $this->checkNbProduct($week);
+        if ($checkNbProducts === TRUE) { //check nb product
+            $checkPicturesOfProducts = $this->checkPictures($week);
+            if ($checkPicturesOfProducts === TRUE) { //check picture file exist
+                return TRUE;
+            } else {
+                $this->error[] = $checkPicturesOfProducts;
+            }
+        } else {
+            $this->error[] = $checkNbProducts;
+        }
+
+        return FALSE;
+    }
+
+    private function checkNbProduct($week)
+    {
+        $countNbProductInCsvContent = count($this->csvContent[$week]);
+        if ($countNbProductInCsvContent < self::NB_REQUIRED_PRODUCTS) { //Missing selected products on Navision
+            $nbMissingProduct = self::NB_REQUIRED_PRODUCTS - (int)$countNbProductInCsvContent;
+
+            $strTxt = "Il manque %d produit(s) pour la semaine %s.\n "
+                ."Il faut sélectionner au moins %d produits par semaine sur Navision.\n "
+                ."Seulement %d produits ont été sélectionnés pour la semaine.\n";
+
+            $obj = "Manque produit(s)";
+            $txt = sprintf($strTxt, $nbMissingProduct, $week, self::NB_REQUIRED_PRODUCTS, $countNbProductInCsvContent);
+
+            return $this->returnAlertParam($obj, $txt);
+        }
+
+        return TRUE;
+    }
+
+    private function checkPictures($week)
+    {
+        $productsOfTheWeek = $this->getProductsToDisplay($week, self::NB_REQUIRED_PRODUCTS);
+        $missing = null;
+
+        foreach ($productsOfTheWeek as $product) {
+            if (empty($product[self::KEY_IMG_PATH])) $missing[] = $product[$this->helperFct::KEY_SKU];
+        }
+
+        if (is_null($missing)) {
+            return TRUE;
+        } else {
+            $strTxt = "Il manque %d image(s) pour la semaine %s.\n "
+                ."Il faut se tourner vers le PAO afin de voir avec eux où en est la demande.\n "
+                ."Photo(s) manquante(s) : %s\n";
+
+            $obj = "Manque image(s)";
+            $txt = sprintf($strTxt, count($missing), $week, print_r($missing, true));
+
+            return $this->returnAlertParam($obj, $txt);
+        }
+    }
+
+    //Display product on template list + use to check img
+    public function getProductsToDisplay(string $week, int $nbProducts): ?array
+    {
+        $productsOfTheWeek = $this->csvContent[strtoupper($week)]; //get sku of the given week
 
         $i = 0;
-        foreach ($productsOfTheWeek as $sku) {
-            $imgsProducts[$sku] = $this->getPictureFileNameOfSku($sku);
+        foreach ($productsOfTheWeek as $product) {
+            //add picture path (key img) to product array (already set : sku, desc)
+            $product[self::KEY_IMG_PATH] = $this->helperFct->getPictureFileNameOfSku($product[$this->helperFct::KEY_SKU]);
+            $productsToDisplay[] = $product;
+
             if (++$i == $nbProducts) break;
         }
 
-        return $imgsProducts;
+        return $productsToDisplay;
     }
 
-    public function checkProducts(): void
+    private function returnAlertParam($obj, $txt): array
     {
-        $csvContent = $this->getCsvContent();
-        $this->setError($csvContent, 'S0');
-        $this->setError($csvContent, 'S1');
-
-        $this->alertTeam();
+        return [
+            self::KEY_ALERT_OBJ => $obj,
+            self::KEY_ALERT_TXT => $txt
+        ];
     }
 
-    private function setError(array $csvContent, string $week): void
-    {
-        $count = count($csvContent[strtoupper($week)]);
-        if ($count < self::NB_REQUIRED_PRODUCTS) {
-            $missing = self::NB_REQUIRED_PRODUCTS - (int)$count;
-            $textProducts = "Il manque %d produit(s) pour la semaine %s \n";
-            $this->error .= sprintf($textProducts, $missing, strtoupper($week));
-        }
-
-        $listSkuWeek = array_slice($csvContent[$week], 0, self::NB_REQUIRED_PRODUCTS, true);
-        foreach ($listSkuWeek as $sku) {
-            $pictureFileName = $this->getPictureFileNameOfSku($sku);
-            if (empty($pictureFileName)) {
-                $textPicture = "Il manque l'image pour le SKU %d de la semaine %s \n";
-                $this->error .= sprintf($textPicture, $sku, $week);
-            }
-        }
-    }
-
-    private function alertTeam(): void
+    public function alertTeam()
     {
         if (!empty($this->error)) {
+            $objs = [];
+            $txts = [];
+            foreach ($this->error as $err) {
+                $objs[] = $err[self::KEY_ALERT_OBJ];
+                $txts[] = $err[self::KEY_ALERT_TXT];
+            }
+
+            $obj = "[C2E] ".implode(" - ",array_unique($objs));
+            $msg = implode("---\n", $txts)."\nDernière mise à jour du fichier CSV : ".$this->lastUpdatedCsv;
+            $to = self::EMAIL_ALERT;
+            $cc = self::EMAIL_TEAM;
+
+            $this->sendMail($obj, $msg, $to, $cc);
+        }
+    }
+
+    private function sendMail($obj, $txt, $to, $cc = ""): void
+    {
+        if (!empty($obj) && !empty($txt)) {
             $mailManager = \Drupal::service('plugin.manager.mail');
 
             $module = 'c2e_produits';
             $key = 'sku_images';
-            $paramMail['to'] = self::EMAIL_TEAM;
             $langcode = \Drupal::currentUser()->getPreferredLangcode();
+
+            $paramMail['cc'] = $cc;
             $paramMail['from'] = self::EMAIL_TEAM;
-            $paramMail['subject'] = "[C2E] Manque sku produits et/ou images";
-            $paramMail['body'] = "";
+            $paramMail['subject'] = $obj;
+            $paramMail['message'] = $txt;
+
             $reply = false;
             $send = true;
 
-            $mailManager->mail($module, $key, $paramMail['to'], $langcode, $paramMail, $reply, $send);
+            $mailManager->mail($module, $key, $to, $langcode, $paramMail, $reply, $send);
         }
     }
 }

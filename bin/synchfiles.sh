8b3dfb09172ea364e853751672056794048db464
Audrey <a.beziat@cargo-services.fr>
2020-10-05 13:40:53 +0200

    maj syncfiles + synchbd pour récupérer bdd et images de production

--- bin/synchfiles.sh
+++ bin/synchfiles.sh
@@ -1,16 +1,28 @@
 #!/bin/bash
+echo "Quelle environnement ? [production || preproduction] ? "
+read env
+if [[ "$env" == "production" ]]
+then
+    hostname="sitedrupal_prod"
+    envurl="sitevitrine"
+else
+    hostname="sitedrupal_preprod"
+    envurl="preprod-sitevitrine"
+    env="preproduction"
+fi
+
 echo "Saisir le nom du site en local"
 read DIRLOCAL
 
-echo "Saisir le nom du site en distant (preprod)"
+echo "Saisir le nom du site en distant ($env)"
 read DIRDISTANT
 
 if [[ "$DIRLOCAL" != "" && "$DIRDISTANT" != "" ]]
 then
     cp -r  docker/deployment/.ssh/ ~/.ssh/
     chmod -R 600 ~/.ssh/
-    rsync -avzL -e "ssh -p 22922" sitedrupal_preprod@preprod-sitevitrine.cargo-webproject.com:deployment/preproduction/shared/web/sites/$DIRDISTANT/files/ web/sites/$DIRLOCAL/files
-    echo deployment/preproduction/shared/web/sites/$DIRDISTANT/files/ web/sites/$DIRLOCAL/files
+    rsync -avzL -e "ssh -p 22922" $hostname@$envurl.cargo-webproject.com:deployment/$env/shared/web/sites/$DIRDISTANT/files/ web/sites/$DIRLOCAL/files
+    echo deployment/$env/shared/web/sites/$DIRDISTANT/files/ web/sites/$DIRLOCAL/files
     db_host="localhost"
     echo "Quelle est la nom de la base pour le site $DIRDISTANT (preprod) ? "
     read db_preprod
@@ -25,7 +37,7 @@ then
             if [[ "$password" != "" ]]
             then
                 echo "mysql dump de la base $db_preprod $username $password en cours ..."
-                ssh -n -p 22922 sitedrupal_preprod@preprod-sitevitrine.cargo-webproject.com "mysqldump -u $username -p$password $db_preprod">db_preprod.sql
+                ssh -n -p 22922 $hostname@$envurl.cargo-webproject.com "mysqldump -u $username -p$password $db_preprod">db_preprod.sql
                 echo "fin du dump"
             fi
         fi

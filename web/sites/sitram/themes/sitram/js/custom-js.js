2541917a424a842342c2070abb26672f1e9cdb2e
Julien <j.onezime@cargo-services.fr>
2020-11-27 10:58:05 +0100

    WIP homepage + footer

--- web/sites/sitram/themes/sitram/js/custom-js.js
+++ web/sites/sitram/themes/sitram/js/custom-js.js
@@ -4,6 +4,23 @@ jQuery(document).ready(function($) {
         dots: true
     });
 
+    // INSTAGRAM
+    new InstagramFeed({
+        'username': 'sitram_fr',
+        'container': $('.js-instagram'),
+        'display_profile': false,
+        'display_biography': false,
+        'display_gallery': true,
+        'display_captions': true,
+        'callback': null,
+        'styling': true,
+        'items': 8,
+        'items_per_row': 4,
+        'margin': 1,
+        'lazy_load': true,
+        'on_error': console.error
+    });
+
     // FORMULAIRE CONTACT
     if ($('.webform-submission-form').length) {
         // Function

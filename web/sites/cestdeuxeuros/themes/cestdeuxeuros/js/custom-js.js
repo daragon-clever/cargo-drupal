8ae8a9727c122c7f8af550308c320c2d4694b0fd
Nicolas <n.troger@cargo-services.fr>
2021-02-17 16:54:55 +0100

    Modif Newsletter page

--- web/sites/cestdeuxeuros/themes/cestdeuxeuros/js/custom-js.js
+++ web/sites/cestdeuxeuros/themes/cestdeuxeuros/js/custom-js.js
@@ -44,6 +44,16 @@ jQuery(document).ready(function ($) {
         });
     }
 
+
+    if ($('#newsletterform-wrapper').length) {
+        $('#edit-rgpd-allow').after(
+            "<span class='custom-checkbox'>" +
+            "<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' aria-hidden='true' focusable='false'>" +
+            "        <path fill='none' stroke='currentColor' stroke-width='3' d='M1.73 12.91l6.37 6.37L22.79 4.59' /></svg>" +
+            "</span>"
+        );
+    }
+
     // Bind click HP
     $("#homepage-content .right a").click(function (e) {
         e.preventDefault();

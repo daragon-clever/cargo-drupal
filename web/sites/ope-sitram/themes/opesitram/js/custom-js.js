c1aeb2c56d7850a0370ff7d1237ef6cb0a962607
Julien <j.onezime@cargo-services.fr>
2021-03-04 12:02:13 +0100

    WIP inté globale

--- web/sites/ope-sitram/themes/opesitram/js/custom-js.js
+++ web/sites/ope-sitram/themes/opesitram/js/custom-js.js
@@ -1,11 +1,11 @@
-jQuery(document).ready(function($) {
+jQuery(document).ready(function ($) {
     // TEST MOBILE / TAB
     var isTabletOrLess = window.matchMedia("only screen and (max-width: 991px)").matches;
 
     // SLICK
     var gammesSlider = $(".js-gammes-slick");
     var productsSlider = $(".js-slick-products");
-    var instagramSlider = $(".js-insta-slick");
+    var instagramSlider = $("#js-instagram");
 
     gammesSlider.slick({
         infinite: true,
@@ -23,9 +23,7 @@ jQuery(document).ready(function($) {
         instagramSlider.slick({
             infinite: true,
             dots: false,
-            adaptiveHeight: true,
-            prevArrow: $('.js-prev-slick'),
-            nextArrow: $('.js-next-slick')
+            adaptiveHeight: true
         });
     }
 
@@ -36,10 +34,10 @@ jQuery(document).ready(function($) {
     });
 
     // SMOOTH SCROLL
-    $(function() {
+    $(function () {
         // ON LOAD
         function smoothScrollTo(target) {
-            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
 
             if (target.length) {
                 $('html,body').animate({
@@ -47,22 +45,23 @@ jQuery(document).ready(function($) {
                 }, 1500);
             }
         }
+
         if (location.hash) {
             window.scrollTo(0, 0);
             target = location.hash.split('#');
-            smoothScrollTo($('#'+target[1]));
+            smoothScrollTo($('#' + target[1]));
         }
 
         // ON CLICK
-        $("a[href*='#']:not([href='#']):not('.navbar-toggler')").click(function() {
+        $("a[href*='#']:not([href='#']):not('.navbar-toggler')").click(function () {
             if (
                 location.hostname == this.hostname
-                && this.pathname.replace(/^\//,"") == location.pathname.replace(/^\//,"")
+                && this.pathname.replace(/^\//, "") == location.pathname.replace(/^\//, "")
             ) {
                 var anchor = $(this.hash);
-                anchor = anchor.length ? anchor : $("[name=" + this.hash.slice(1) +"]");
-                if ( anchor.length ) {
-                    $("html, body").animate( { scrollTop: anchor.offset().top }, 1500);
+                anchor = anchor.length ? anchor : $("[name=" + this.hash.slice(1) + "]");
+                if (anchor.length) {
+                    $("html, body").animate({scrollTop: anchor.offset().top}, 1500);
                 }
             }
         });
@@ -70,7 +69,7 @@ jQuery(document).ready(function($) {
 
     // GESTION LIEN STORELOCATOR
     var storeLocatorLink = $("#block-navigationprincipale .storelocator-main-link");
-    if ( window.location.pathname == '/' ){
+    if (window.location.pathname == '/') {
         storeLocatorLink
             .attr("href", "#js-store-locator")
             .removeClass("is-active");
@@ -79,23 +78,39 @@ jQuery(document).ready(function($) {
     }
 
     // INSTAGRAM
-    (function () {
-        new InstagramFeed({
-            'username': 'sitram_fr',
-            'container': document.getElementById("js-instagram"),
-            'display_profile': false,
-            'display_biography': false,
-            'display_gallery': true,
-            'display_captions': false,
-            'callback': null,
-            'styling': true,
-            'items': 4,
-            'items_per_row': 4,
-            'margin': 1,
-            'lazy_load': true,
-            'on_error': function () {
-                $('#js-instagram').html('<a href="https://www.instagram.com/sitram_fr" target="_blank"><img class="mw-100" src="/sites/ope-sitram/themes/opesitram/images/footer/fake-insta.png" /></a>')
-            }
-        });
-    })();
+    if ($('#js-instagram').length) {
+        (function () {
+            new InstagramFeed({
+                'username': 'sitram_fr',
+                'container': document.getElementById("js-instagram"),
+                'display_profile': false,
+                'display_biography': false,
+                'display_gallery': true,
+                'display_captions': false,
+                'callback': null,
+                'styling': true,
+                'items': 4,
+                'items_per_row': 4,
+                'margin': 1,
+                'lazy_load': true,
+                'on_error': function () {
+                    $('#js-instagram').html('<a href="https://www.instagram.com/sitram_fr" target="_blank"><img class="mw-100" src="/sites/ope-sitram/themes/opesitram/images/footer/fake-insta.png" /></a>')
+                }
+            });
+        })();
+    }
+
+    // POPUP VIDEO
+    $('.js-open-video').click(function (e) {
+        e.preventDefault();
+        $(this).next('.js-hidden-video').fadeIn();
+    });
+
+    $('.js-close-hidden-video').click(function () {
+        const iframe = $(this).prev();
+        const src = iframe.attr('src');
+        $(this).parents('.js-hidden-video').fadeOut();
+        iframe.attr('src', '');
+        iframe.attr('src', src)
+    });
 });

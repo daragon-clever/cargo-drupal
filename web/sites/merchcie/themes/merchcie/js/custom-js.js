ab062c3a229b926103627e941de1d2b29f178e38
Nicolas <n.troger@cargo-services.fr>
2021-03-17 17:06:24 +0100

    WIP - UI Kit

--- web/sites/merchcie/themes/merchcie/js/custom-js.js
+++ web/sites/merchcie/themes/merchcie/js/custom-js.js
@@ -1,4 +1,10 @@
 jQuery(document).ready(function($) {
     // TEST MOBILE / TAB
     var isTabletOrLess = window.matchMedia("only screen and (max-width: 991px)").matches;
+
+    /** UI KIT **/
+    document.querySelectorAll("code").forEach(function(element) {
+        element.innerHTML = element.innerHTML.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/"/g, "&quot;").replace(/'/g, "&#039;");
+    });
+    hljs.highlightAll();
 });

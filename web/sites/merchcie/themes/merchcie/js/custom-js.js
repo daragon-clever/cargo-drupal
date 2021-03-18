3c4cd669db1b7f18290a29148f10d1fdc444f28a
Nicolas <n.troger@cargo-services.fr>
2021-03-18 13:51:33 +0100

    Header desktop

--- web/sites/merchcie/themes/merchcie/js/custom-js.js
+++ web/sites/merchcie/themes/merchcie/js/custom-js.js
@@ -2,6 +2,16 @@ jQuery(document).ready(function ($) {
     // TEST MOBILE / TAB
     var isTabletOrLess = window.matchMedia("only screen and (max-width: 991px)").matches;
 
+    // STICKY HEADER
+    $(window).scroll(function(){
+        var header = $('header');
+        if ($(window).scrollTop() >= 1) {
+            header.addClass('fixed');
+        } else {
+            header.removeClass('fixed');
+        }
+    });
+
     /** UI KIT **/
     if ($("#ui-kit").length) {
         document.querySelectorAll("code").forEach(function (element) {

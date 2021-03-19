4a30ed1344dc6861dd1f58ede61b6fafc11d5887
Nicolas <n.troger@cargo-services.fr>
2021-03-19 16:23:46 +0100

    HP : Testimonials + js increment number animation

--- web/sites/merchcie/themes/merchcie/js/custom-js.js
+++ web/sites/merchcie/themes/merchcie/js/custom-js.js
@@ -39,6 +39,33 @@ jQuery(document).ready(function ($) {
             video.fadeToggle();
             $(this).hasClass("playing") ? video.get(0).pause() : video.get(0).play();
             $(this).toggleClass("playing");
-        })
+        });
+
+        // JS number animation
+        Number.prototype.format = function(n) {
+            var r = new RegExp('\\d(?=(\\d{3})+' + (n > 0 ? '\\.' : '$') + ')', 'g');
+            return this.toFixed(Math.max(0, Math.floor(n)));
+        };
+
+        $('.text-num').each(function () {
+            $(this).prop('counter', 0).animate({
+                counter: $(this).text()
+            }, {
+                duration: 10000,
+                // easing: 'easeOutExpo',
+                step: function (step) {
+                    $(this).text('' + step.format());
+                }
+            });
+        });
     }
+
+    // TESTIMONIALS
+    $(".js-testimonials-slick").slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 5000
+    });
 });

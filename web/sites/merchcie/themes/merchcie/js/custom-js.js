52d91351589ecfe83226abeabd09b361881e31c6
Nicolas <n.troger@cargo-services.fr>
2021-03-22 14:41:59 +0100

    Tests responsive

--- web/sites/merchcie/themes/merchcie/js/custom-js.js
+++ web/sites/merchcie/themes/merchcie/js/custom-js.js
@@ -66,6 +66,20 @@ jQuery(document).ready(function ($) {
         slidesToShow: 3,
         slidesToScroll: 1,
         autoplay: true,
-        autoplaySpeed: 5000
+        autoplaySpeed: 5000,
+        responsive: [
+            {
+                breakpoint: 990,
+                settings: {
+                    slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 767,
+                settings: {
+                    slidesToShow: 1
+                }
+            }
+        ]
     });
 });

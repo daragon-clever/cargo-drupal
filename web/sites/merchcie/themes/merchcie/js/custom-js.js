704ec929dee5cfb5760baf3b6fe3c7c9b62e9357
Nicolas <n.troger@cargo-services.fr>
2021-04-08 16:54:17 +0200

    Fix form label on focus

--- web/sites/merchcie/themes/merchcie/js/custom-js.js
+++ web/sites/merchcie/themes/merchcie/js/custom-js.js
@@ -160,4 +160,29 @@ jQuery(document).ready(function ($) {
         autoplaySpeed: 1000,
         cssEase: 'linear'
     });
+
+    // FORMS
+    if ($('.webform-submission-form').length) {
+        // Function
+        var checkValue = function (e) {
+            tmpval = e.val();
+            if (tmpval === '') {
+                e.addClass('js-empty').removeClass('js-not-empty');
+            } else {
+                e.addClass('js-not-empty').removeClass('js-empty');
+            }
+        };
+        // Var Elts
+        var formElts = $('input, textarea');
+        // 1er chargement
+        formElts.each(function () {
+            checkValue($(this));
+        });
+        // Au clic sur l'elt
+        formElts.blur(function () {
+            checkValue($(this));
+        });
+        // Required
+        $(".required").parent().addClass("required-wrapper");
+    }
 });

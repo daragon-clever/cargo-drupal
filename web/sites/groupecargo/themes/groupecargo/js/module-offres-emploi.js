ca5b7673c2e5ee504831d78a7164293dc0cdb395
Nicolas <n.troger@cargo-services.fr>
2021-06-23 10:46:38 +0200

    Fix - Gers Equipement

--- web/sites/groupecargo/themes/groupecargo/js/module-offres-emploi.js
+++ web/sites/groupecargo/themes/groupecargo/js/module-offres-emploi.js
@@ -54,7 +54,7 @@ jQuery(document).ready(function($) {
                     type: 'column',
                     target: 'tr'
                 }
-            },//responsive
+            },
 
             //filters
             initComplete: function () {

cb111da2d6452ce233c1a12fd68fa47137dc9764
Franck Fernandez <ffernandez@clever-age.com>
2022-02-09 18:08:40 +0100

    fix #18 #17

--- web/sites/sema-design/modules/sema_core/src/Controller/NewsletterController.php
+++ web/sites/sema-design/modules/sema_core/src/Controller/NewsletterController.php
@@ -4,20 +4,25 @@
  * @file
  * Controller file for sema_core module.
  */
+
 namespace Drupal\sema_core\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
 
-class NewsletterController extends ControllerBase {
+class NewsletterController extends ControllerBase
+{
 
-  public function newsletter() {
+  public function newsletter()
+  {
     $datas = [];
 
     $myConfigPage = \Drupal\config_pages\Entity\ConfigPages::config('page_newsletter');
     if (!empty($myConfigPage)) {
       $datas['description'] = $myConfigPage->get('field_description')->value;
       $datas['title'] = $myConfigPage->get('field_title')->value;
-      $datas['mainPicture'] = $myConfigPage->get('field_main_visual')->view();
+      $view_builder = \Drupal::entityTypeManager()
+        ->getViewBuilder($myConfigPage->get('field_main_visual')->entity->getEntityTypeId());
+      $datas['mainPicture'] = $view_builder->view($myConfigPage->get('field_main_visual')->entity, 'default');
     }
 
     return [
